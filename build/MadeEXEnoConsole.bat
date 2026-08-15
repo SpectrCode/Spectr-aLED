7b9ff4a907fc85351d39862ebf0a5be1a38db163
SpectrCode <realism4youui@gmail.com>
2026-08-16 00:19:43 +0300

    Delete build/MadeEXEnoConsole.bat

--- build/MadeEXEnoConsole.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Building Spectr_aLED as a folder (onedir mode, windowed)...
-cd ..
-pyinstaller --clean --noconfirm build\Spectr_aLED.spec
-echo.
-echo Build complete! Output is in: dist\SpectrLed\
-echo This folder contains Spectr_aLEDd.exe + all dependencies.
-pause
